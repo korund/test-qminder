fdcf3238723ced2bfab4f4e873675d752632de9b
Rudolf Konstantinov <korund93@gmail.com>
2018-10-07 23:28:37 +0300

    Updates

---
+++ precache-manifest.4deff2940a84f54dfd338448e0fef2e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c6071c98ed20c87413c7",
+    "url": "/static/js/main.c6071c98.chunk.js"
+  },
+  {
+    "revision": "5d34a58b92977b547983",
+    "url": "/static/js/1.5d34a58b.chunk.js"
+  },
+  {
+    "revision": "c6071c98ed20c87413c7",
+    "url": "/static/css/main.1e086a3a.chunk.css"
+  },
+  {
+    "revision": "322e24ab7634770dd7cd76bb58b06859",
+    "url": "/index.html"
+  }
+];
